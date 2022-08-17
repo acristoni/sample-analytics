4262857e13a3375052a1cab47cb0ded83609fc83
acristoni <adonai_cn@hotmail.com>
2022-08-17 15:19:22 -0300

    hadle create user

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserEvent } from './create-user.events';
 
 @Injectable()
 export class AppService {
+  private readonly analytics: any[] = [];
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  handleUserCreated(data: CreateUserEvent) {
+    console.log('handleUserCreated - ANALYTICS', data);
+    this.analytics.push({
+      email: data.email,
+      timestamp: new Date(),
+    });
   }
 }
