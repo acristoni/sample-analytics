fb40406b387efa7abbd23c99f8be0b0558b9bea2
acristoni <adonai_cn@hotmail.com>
2022-08-17 16:08:49 -0300

    response between microservices done

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,17 @@
 import { NestFactory } from '@nestjs/core';
-import { Transport } from '@nestjs/microservices';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.connectMicroservice({
-    transport: Transport.TCP,
-    options: {
-      port: 3001,
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: 3001
+      }
     },
-  });
-  await app.startAllMicroservices();
-  await app.listen(3001);
+  );
+  await app.listen();
 }
 bootstrap();
